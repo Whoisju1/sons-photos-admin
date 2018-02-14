5a0b05582e0f17fb579a0780c081672f96a7b4f7
Juan John-Charles <whoisju1@gmail.com>
2018-02-13 22:23:21 -0500

    disable eslint rule in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import baseStyles from './base-styles';
+
+/* eslint react/prefer-stateless-function:[ "off"] */
 class App extends Component {
   render() {
     baseStyles();
-    
+
     const RootElement = styled.div`
       min-width: 100vw;
       min-height: 100vh;

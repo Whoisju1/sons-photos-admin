26e29a93a82e7ecac6922f4ce30e9bd76e807b86
Juan John-Charles <whoisju1@gmail.com>
2018-02-13 20:37:08 -0500

    set up basic server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));

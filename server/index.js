db780b70eb56acd5c359e09ab1f75ac064848c69
Juan John-Charles <whoisju1@gmail.com>
2018-02-13 20:56:18 -0500

    set up graphiql to work with express-graphql

--- server/index.js
+++ server/index.js
@@ -4,4 +4,8 @@ const graphqlHTTP = require('express-graphql');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+}))
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));

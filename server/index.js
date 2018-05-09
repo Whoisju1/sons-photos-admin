063a59d2723f265a948dc4e65675238a87be5451
Juan John-Charles <whoisju1@gmail.com>
2018-05-09 13:38:57 -0400

    set up scripts to run app

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const { PORT } = process.env;
 
 app.use('/graphql', graphqlHTTP({
   graphiql: true,
-}))
+}));
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));

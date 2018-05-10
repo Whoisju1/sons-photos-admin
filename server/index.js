f8f2903b9b60e85b1c7fd1819d1df76206618466
Juan John-Charles <whoisju1@gmail.com>
2018-05-10 10:24:25 -0400

    user babel to compile ES6 code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+require('dotenv').config(); // eslint-disable-line
 
 const app = express();
 const { PORT } = process.env;

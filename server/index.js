c52fa45910f44e7aecc3eca5c03d80bd987cc0fa
Juan John-Charles <whoisju1@gmail.com>
2018-05-10 11:10:28 -0400

    use morgan for logging

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import logger from 'morgan';
 
 require('dotenv').config(); // eslint-disable-line
 
 const app = express();
 const { PORT } = process.env;
+
+// ues middleware
+app.use(logger('dev'));
 
 app.use('/graphql', graphqlHTTP({
   graphiql: true,

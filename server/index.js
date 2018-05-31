b402334463359c87406bcd699182ebd3c0e3f44e
Juan John-Charles <whoisju1@gmail.com>
2018-05-31 09:38:24 -0400

    make explantory comments in server.js

--- server/index.js
+++ server/index.js
@@ -6,26 +6,29 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import resolvers from '../resolvers';
 
-require('dotenv').config(); // eslint-disable-line
+require('dotenv').config();
 
 const app = express();
 const { PORT } = process.env;
 
 // ues middleware
+// use morgan for logging to request details to the console
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
 
+// get schema from graphql file using fs module
 const typeDefs = fs.readFileSync('schema.graphql', { encoding: 'utf8' });
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
+// set up graphql endpoint
 app.use('/graphql', graphqlExpress(request => ({
   schema,
   context: {
-    request,
+    request, // the express request object
   },
 })));
 
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // use graphiql for testing graphql endpoint
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`)); // eslint-disable-line no-console

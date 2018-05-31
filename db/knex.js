8867a7d2a7d1866a665f30814d284090dc53ba9a
Juan John-Charles <whoisju1@gmail.com>
2018-05-31 11:32:20 -0400

    refactor the configeration for knex using es6

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,6 @@
+import knex from 'knex';
+import knexFile from '../knexfile';
+
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile')[environment];
-module.exports = require('knex')(configuration);
+const configuration = knexFile[environment];
+export default knex(configuration);

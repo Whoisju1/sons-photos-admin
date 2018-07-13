43ef559438bd08eb222a1ec749614ff91dbcc300
Juan John-Charles <whoisju1@gmail.com>
2018-07-12 22:47:10 -0400

    setup resolver to retrieve gallery

--- resolvers/gallery.js
+++ resolvers/gallery.js
@@ -1,3 +1,14 @@
-const gallery = async () => null;
+const gallery = async (root, { galleryID }, { db, request: { user = null } }) => {
+  try {
+    if (!user) return new Error('Please sign in');
+    const [galleryInfo] = await db('gallery_view')
+      .select()
+      .where({ galleryID });
+
+    return galleryInfo;
+  } catch (err) {
+    return err;
+  }
+};
 
 export default gallery;

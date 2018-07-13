b06c1d3cd88a030a61f18a26bcf82cccf9d5a74e
Juan John-Charles <whoisju1@gmail.com>
2018-07-12 22:34:56 -0400

    create galleries resolver

--- resolvers/galleries.js
+++ resolvers/galleries.js
@@ -1,3 +1,13 @@
-const galleries = async () => null;
+const galleries = async (root, args, { db, request }) => {
+  try {
+    if (!request.user) return new Error('Please sign in');
+    const galleriesList = await db('gallery_view')
+      .select();
+
+    return galleriesList;
+  } catch (err) {
+    return err;
+  }
+};
 
 export default galleries;

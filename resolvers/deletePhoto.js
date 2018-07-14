f3096c486324a0a48f19f2ba72a04cf020815333
Juan John-Charles <whoisju1@gmail.com>
2018-07-14 11:59:28 -0400

    write logic for deleting photo

--- resolvers/deletePhoto.js
+++ resolvers/deletePhoto.js
@@ -1,3 +1,17 @@
-const deletePhoto = async () => null;
+/* eslint-disable camelcase */
+const deletePhoto = async (root, { photo_id }, { db, request: { user } }) => {
+  try {
+    if (!user) return new Error('Please sign in');
+    const res = await db('photo')
+      .where({ photo_id })
+      .del()
+      .returning('*');
+
+    console.log(res);
+    return null;
+  } catch (err) {
+    return err;
+  }
+};
 
 export default deletePhoto;

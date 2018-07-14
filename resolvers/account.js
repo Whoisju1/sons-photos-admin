ddf7314efcfae9adcda6bb5f62904285914592ed
Juan John-Charles <whoisju1@gmail.com>
2018-07-14 17:12:41 -0400

    remove uneeded authourization in resolver

--- resolvers/account.js
+++ resolvers/account.js
@@ -1,7 +1,6 @@
 import requireAuth from '../resolverMiddleware/requireAuth';
 
 const account = async (root, args, { db, request: { user } }) => {
-  if (!user) return new Error('Please sign in');
   // get user id
   const { account_id: accountID } = user.sub; // eslint-disable-line camelcase
   const [accountInfo] = await db

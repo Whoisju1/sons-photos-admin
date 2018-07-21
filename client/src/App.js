b7267256c8ca6c65b7d7185e65a59391dd89b0b4
Juan John-Charles <whoisju1@gmail.com>
2018-07-20 23:46:01 -0400

    remove default react setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello World
-      </div>
-    );
-  }
-}
+const App = () => <div>Sons Photos</div>;
 
 export default App;

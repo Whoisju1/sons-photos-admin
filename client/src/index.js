86ab356eca390f52b3ffdc1679ede3f6669b9ff2
Juan John-Charles <whoisju1@gmail.com>
2018-07-20 23:55:40 -0400

    setup base apollo-client configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const SONS_PHOTOS_URI = '/graphql';
+
+const httpLink = new HttpLink({
+  uri: SONS_PHOTOS_URI,
+  headers: {
+    authorization: `Bearer ${localStorage.get('token')}`,
+  },
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

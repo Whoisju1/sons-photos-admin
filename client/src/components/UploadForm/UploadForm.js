6821be1fa37baeadbecd12db5e531dfa022630c7
Juan John-Charles <whoisju1@gmail.com>
2018-08-02 23:56:35 -0400

    write client side code for uploading photo

--- client/src/components/UploadForm/UploadForm.js
+++ client/src/components/UploadForm/UploadForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ApolloConsumer } from 'react-apollo';
 import gql from 'graphql-tag';
+import axios from 'axios';
 import Form from '../../blocks/Form/index';
 
 const GET_PRESIGNED_URL = gql`
@@ -8,6 +9,18 @@ const GET_PRESIGNED_URL = gql`
     s3PreSignedURL (filename: $filename) {
       url
       key
+    }
+  }
+`;
+
+const ADD_PHOTO = gql`
+  mutation StorePhoto ($photoInfo:photoInput) {
+    addPhoto (input:$photoInfo) {
+      photoID
+      url
+      description
+      clickCount
+      createdAt
     }
   }
 `;
@@ -20,7 +33,7 @@ class UploadForm extends Component {
 
   handleChange = ({ target }) => {
     const { files } = target;
-    this.setState((state, props) => ({
+    this.setState(() => ({
       file: files[0],
       filename: files[0].name,
     }));
@@ -34,15 +47,39 @@ class UploadForm extends Component {
             <Form
               onSubmit={
                 async (event) => {
-                  const { filename } = this.state;
-                  event.preventDefault();
-                  const preSignedURL = await client.query({
-                    query: GET_PRESIGNED_URL,
-                    variables: {
-                      filename,
-                    },
-                  });
-                  console.log(preSignedURL);
+                  try {
+                    event.preventDefault();
+                    const { filename, file } = this.state;
+                    // get pre-signed url from aws
+                    const preSignedURL = await client.query({
+                      query: GET_PRESIGNED_URL,
+                      variables: {
+                        filename,
+                      },
+                    });
+                    const { url, key } = preSignedURL.data.s3PreSignedURL;
+                    await axios.put(url, file, {
+                      headers: {
+                        'Content-Type': file.type,
+                      },
+                    });
+
+                    const photoInfo = await client.mutate({
+                      mutation: ADD_PHOTO,
+                      variables: {
+                        photoInfo: {
+                          url: key,
+                          gallery_id: 11,
+                          photo_description: 'Some description',
+                        },
+                      },
+                    });
+
+                    console.log(photoInfo);
+                    return photoInfo;
+                  } catch (err) {
+                    return err;
+                  }
                 }
               }
             >

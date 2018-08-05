23dee849abc9471b3872f3318f20ed989b917df4
Juan John-Charles <whoisju1@gmail.com>
2018-08-05 15:22:11 -0400

    setup apollo in index.ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const SONS_PHOTOS_URI = '/graphql';
+
+const httpLink = new HttpLink({
+  uri: SONS_PHOTOS_URI,
+  headers: {
+    authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();

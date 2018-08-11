4a943963a50d2659da03d92c041c2f6b41639022
Juan John-Charles <whoisju1@gmail.com>
2018-08-11 12:29:53 -0400

    remove instance of text component in section

--- client/src/components/Section/Section.tsx
+++ client/src/components/Section/Section.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Text } from '../GeneralComponents';
 import GalleryList from './GalleryList';
 
 const SectionElm = styled.section`
@@ -10,7 +9,6 @@ const SectionElm = styled.section`
 const Section: React.SFC<{}> = () => {
   return (
     <SectionElm>
-      <Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ratione, temporibus, molestias itaque eos vero autem beatae aliquam consequuntur inventore, ullam nostrum? Sint corporis possimus molestias laudantium voluptatum aperiam quisquam assumenda labore nobis hic nisi eligendi ullam, officia ratione aliquam veniam necessitatibus aliquid. Impedit maiores delectus eum, quisquam libero ut! </Text>
       <GalleryList />
     </SectionElm>
   );

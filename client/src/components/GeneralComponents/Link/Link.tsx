ed0ee8e16dabac52ce759472da651400d39bcf4e
Juan John-Charles <whoisju1@gmail.com>
2018-08-11 12:18:11 -0400

    put general components into their own folders

--- client/src/components/GeneralComponents/Link/Link.tsx
+++ client/src/components/GeneralComponents/Link/Link.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link, LinkProps } from 'react-router-dom';
-import styled from '../../styled-components';
+import styled from '../../../styled-components';
 
 const StyledLink = styled(Link)<{}>`
   

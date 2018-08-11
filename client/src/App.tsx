39e7833bb3c9eb9284b67ef5f412d58eaaf1a4c5
Juan John-Charles <whoisju1@gmail.com>
2018-08-11 17:24:11 -0400

    set global styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,25 @@ import Header from './components/Header/Header';
 import Section from './components/Section';
 import SideNav from './components/SideNav';
 
+import { injectGlobal } from './styled-components';
+
+injectGlobal`
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    box-sizing: border-box;
+  }
+`;
+
 class App extends React.Component {
   public render() {
     return (

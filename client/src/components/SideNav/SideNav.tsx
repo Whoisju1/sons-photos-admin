9514738cf06f6291b7dfd84f9587fbc873e45241
Juan John-Charles <whoisju1@gmail.com>
2018-08-11 16:16:30 -0400

    add unhandled routes to SideNav

--- client/src/components/SideNav/SideNav.tsx
+++ client/src/components/SideNav/SideNav.tsx
@@ -11,8 +11,21 @@ export default () => (
     <Heading headingType={'secondary'}>
       Side Nav
     </Heading>
+    <hr/>
+    <Link to={'/account'} >
+      Account
+    </Link>
+    <hr/>
+    <Link to={'/company'} >
+      Company
+    </Link>
+    <hr/>
     <Link to={'/galleries'} >
       Galleries
+    </Link>
+    <hr/>
+    <Link to={'/stats'} >
+      Stats
     </Link>
   </SideNav>
 );

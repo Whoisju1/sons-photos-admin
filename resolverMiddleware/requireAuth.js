b36d7ad33a51158b05468c2fe64404d241e02988
Juan John-Charles <whoisju1@gmail.com>
2018-09-23 22:28:35 -0400

    add more conditions to authentication middleware

--- resolverMiddleware/requireAuth.js
+++ resolverMiddleware/requireAuth.js
@@ -1,6 +1,26 @@
 import { skip, combineResolvers } from 'graphql-resolvers';
+import db from '../db/knex';
 
-const isAuthenticated = (root, args, { req: { user = null } }) => (user ? skip : new Error('Unauthorized!'));
+const isAuthenticated = async (root, args, { req: { user = null } }) => {
+  try {
+    if (!user) return new Error('Unauthorized!');
+
+    const { accountID } = user.sub; // eslint-disable-line camelcase
+    const foundUser = await db
+      .select('accountID')
+      .where({ accountID })
+      .from('account');
+
+    // user doesn't exist in database return an error of Unauthorized
+    if (!foundUser.length) return new Error('Unauthorized!');
+
+    return user
+      ? skip
+      : new Error('Unauthorized!');
+  } catch (err) {
+    return err;
+  }
+};
 
 export default resolver => combineResolvers(
   isAuthenticated,

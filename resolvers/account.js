6d4fb06be86e98ee93623d720b899746dd6b0581
Juan John-Charles <whoisju1@gmail.com>
2018-09-23 22:29:11 -0400

    add error handling to account resolver

--- resolvers/account.js
+++ resolvers/account.js
@@ -1,14 +1,18 @@
 import requireAuth from '../resolverMiddleware/requireAuth';
 
 const account = async (root, args, { db, req: { user } }) => {
-  // get user id
-  const { accountID } = user.sub; // eslint-disable-line camelcase
-  const [accountInfo] = await db
-    .select()
-    .where({ accountID })
-    .from('account');
+  try {
+    // get user id
+    const { accountID } = user.sub; // eslint-disable-line camelcase
+    const [accountInfo] = await db
+      .select()
+      .where({ accountID })
+      .from('account');
 
-  return accountInfo;
+    return accountInfo;
+  } catch (err) {
+    return err;
+  }
 };
 
 export default requireAuth(account);

de4f52526fcf9574fb3b9e530b373534b074a07e
Juan John-Charles <whoisju1@gmail.com>
2018-10-17 00:14:21 -0400

    configure tsconfig

--- db/knex.ts
+++
@@ -1,6 +0,0 @@
-import knex from 'knex';
-import knexFile from '../knexfile';
-
-const environment = process.env.NODE_ENV || 'development';
-const configuration = knexFile[environment];
-export default knex(configuration);

d2759200df96905114b5297445e6d26fde3c2974
Juan John-Charles <whoisju1@gmail.com>
2018-12-30 00:44:49 -0500

    remove console.log

--- src/services/tokenCreation.ts
+++ src/services/tokenCreation.ts
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRETE as secrete } from '../config';
 
 export const generateToken = <T>(sub: T) => {
-  console.log({ sub });
   const token: string = jwt.sign({ sub }, secrete);
   return token;
 };
